403a4cb1f89eaa22d711e5530f89328ce2c35921
emartinsmiron@gmail.com <eduardo.silva@mult-e.com.br>
2019-06-02 22:16:42 -0300

    refactoring packages service and infra

---
+++ src/main/java/br/com/emiron/fileservice/service/FileService.java
@@ -0,0 +1,34 @@
+package br.com.emiron.fileservice.service;
+
+import br.com.emiron.fileservice.infra.S3Client;
+import com.amazonaws.util.Md5Utils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MimeType;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+
+    private final S3Client client;
+
+    public ResponseEntity upload(MultipartFile multipartFile) {
+        try {
+            return new ResponseEntity(client.store(multipartFile.getBytes(),
+                        Md5Utils.md5AsBase64(multipartFile.getBytes()),
+                        MimeType.valueOf(multipartFile.getContentType())),HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity getFileUrl(String key){
+        return new ResponseEntity(client.getFileUrl(key), HttpStatus.OK);
+
+    }
+}
